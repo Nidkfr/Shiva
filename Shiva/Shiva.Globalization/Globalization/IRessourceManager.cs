16098e3d4c0708e2cb16b8b975f306e8135b5910
Adrien anfossi <adrien.anfossi@akanea.com>
2018-01-10 23:42:04 +0100

    Start Implementation of Globalization

---
+++ Shiva/Shiva.Globalization/Globalization/IRessourceManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Shiva.Globalization
+{
+    /// <summary>
+    /// Service to manage ressource globalization
+    /// </summary>
+    public interface IRessourceManager
+    {
+        /// <summary>
+        /// Loads All string from culture in merory.
+        /// </summary>
+        /// <param name="culture">The culture, if null then current culture of ressourceManager.</param>
+        /// <returns>IRessrouceStringService</returns>
+        Task<IRessourceStringService> LoadStringService(CultureInfo culture=null);
+
+        /// <summary>
+        /// Change the current culture.
+        /// </summary>
+        /// <param name="culture">The culture.</param>
+        void SetCurrentCulture(CultureInfo culture);
+
+        /// <summary>
+        /// Occurs when [current culture changed].
+        /// </summary>
+        event EventHandler<RessourceManagerCultureArgs> CurrentCultureChanged;
+
+        /// <summary>
+        /// Gets the current culture.
+        /// </summary>
+        /// <value>
+        /// The current culture.
+        /// </value>
+        CultureInfo CurrentCulture { get; }
+    }
+
+    /// <summary>
+    /// Event Arg for Ressource Manager
+    /// </summary>
+    /// <seealso cref="System.EventArgs" />
+    public sealed class RessourceManagerCultureArgs : EventArgs
+    {
+
+    }
+}
---
+++ Shiva/Shiva.Globalization/Globalization/IRessourceStringService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shiva.Globalization
+{
+    /// <summary>
+    /// REsource string Service
+    /// </summary>
+    public interface IRessourceStringService
+    {
+        /// <summary>
+        /// Gets the string.
+        /// </summary>
+        /// <param name="Id">The identifier.</param>
+        /// <returns></returns>
+        string GetString(string Id);
+    }
+}
