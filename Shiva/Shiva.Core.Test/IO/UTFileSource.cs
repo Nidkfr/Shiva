d591bbcddc091d1a0667a9df5b31288deb8730b5
Adrien anfossi <adrien.anfossi@akanea.com>
2018-06-06 22:30:14 +0200

    Implement FileSource

---
+++ Shiva/Shiva.Core.Test/IO/UTFileSource.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Shiva.IO
+{
+    [TestClass]
+    [DeploymentItem("DeployItems", "DeployItems")]
+    public class UTFileSource
+    {
+        [TestMethod]
+        public void TestInitializer()
+        {
+            var file = new FileSource("./DeployItems/XmlSample.xml");
+            Assert.IsFalse(file.IsOpen);
+            Assert.IsFalse(file.IsReadOnly);            
+        }
+
+        [TestMethod]
+        public void TestOpen()
+        {
+            var file = new FileSource("./DeployItems/XmlSample.xml");
+            Assert.IsFalse(file.IsOpen);
+            Assert.IsFalse(file.IsReadOnly);
+
+            file.Open();
+
+            Assert.IsTrue(file.IsOpen);
+            Assert.IsFalse(file.IsReadOnly);
+        }
+
+        [TestMethod]
+        public void TestClose()
+        {
+            var file = new FileSource("./DeployItems/XmlSample.xml");
+            file.Open();
+
+            Assert.IsTrue(file.IsOpen);
+            Assert.IsFalse(file.IsReadOnly);
+
+            file.Close();
+
+            Assert.IsFalse(file.IsOpen);
+
+            file.Open();
+            Assert.IsTrue(file.IsOpen);
+        }
+
+        [TestMethod]
+        public void TestDispose()
+        {
+            FileSource file = null;
+            using (file = new FileSource("./DeployItems/XmlSample.xml"))
+            {
+                Assert.IsTrue(file.Stream.CanRead);
+                Assert.IsTrue(file.Stream.CanWrite);
+            }
+            Assert.IsFalse(file.IsOpen);
+        }
+    }
+}
---
+++ Shiva/Shiva.Services.XmlRessources/Ressources/XmlRessourceManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Shiva.Core.Identities;
+using System.IO;
+using System.Xml;
+using Shiva.IO;
+
+namespace Shiva.Ressources
+{
+    /// <summary>
+    /// Xml ressource Manager
+    /// </summary>
+    public class XmlRessourceManager : RessourceManagerBase
+    {
+        private readonly CultureInfo _culture;
+        private readonly StreamSource _source;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XmlRessourceManager"/> class.
+        /// </summary>
+        /// <param name="culture">The culture.</param>
+        public XmlRessourceManager(CultureInfo culture, StreamSource dataXml)
+        {
+            this._culture = culture ?? throw new ArgumentNullException(nameof(culture));
+            this._source = dataXml ?? throw new ArgumentNullException(nameof(dataXml));
+        }
+
+        /// <summary>
+        /// Gets the culture.
+        /// </summary>
+        /// <value>
+        /// The culture.
+        /// </value>
+        public override CultureInfo Culture => this._culture;
+
+
+        public override void AttachRessourceToGroup(Identity ressourceId, Identity groupRessourceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<Identity> GetAllGroups()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IRessourcesGroup<TRessource> GetGroupRessources<TRessource>(Identity groupRessourceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IRessourcesGroup<TRessource> GetGroupRessources<TRessource>(Namespace groupNamespaceRessource)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override TRessource GetRessource<TRessource>(Identity ressourceID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RemoveGroup(Identity groupRessourceId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SaveRessource<TRessource>(TRessource ressource)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
